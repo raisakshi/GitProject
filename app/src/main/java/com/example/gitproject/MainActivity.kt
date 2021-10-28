70dca615e075562ea23116dd99b2f2600a475d91
isaks <sakshirai9557@gmail.com>
2021-10-28 18:27:52 +0530

    Sec Activity Created

--- app/src/main/java/com/example/gitproject/MainActivity.kt
+++ app/src/main/java/com/example/gitproject/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gitproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.Buttons)
+        button.setOnClickListener {
+            val intent=Intent(this, SecActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
